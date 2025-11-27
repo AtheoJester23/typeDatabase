e26c957fe3b806c3f984cb7f1c7c551edc9c16a7
AtheoJester23 <atheojester@gmail.com>
2025-11-28 01:36:59 +0800

    feat: add tls configuration

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -8,6 +8,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL,
     pass: process.env.PASSCODE,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 export function emailSend(to, body) {
