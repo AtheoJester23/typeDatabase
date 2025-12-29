4c08ad8ac7ac56641ab69920e38e7a8635563245
AtheoJester23 <atheojester@gmail.com>
2025-12-30 02:35:16 +0800

    fix: add production frontend origin to CORS

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 //cors:
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "typetype-ten.vercel.app"],
     credentials: true,
   })
 );
